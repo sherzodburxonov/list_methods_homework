bf6fe431e0f3cd219e2d85c704ebac66cf4789c1
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 15:06:18 +0500

    07

--- List07.py
+++ List07.py
@@ -6,4 +6,11 @@ def main(list01):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    n=0
+    while i<len(list01):
+        if list01[i]==0:
+            n=n+1
+        i=i+1 
+    return n
+print(main([1, 0, 1, 1, 0, 1, 1]))
